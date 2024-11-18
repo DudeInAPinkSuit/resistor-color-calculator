82f1fb4b29d620ff059c50956dc9592a65929d84
alecbnono <alecbnono@gmail.com>
2024-11-18 16:22:54 +0800

    Documenting Finish; Submission Ready

--- MP1.c
+++ MP1.c
@@ -11,10 +11,10 @@ NONO, Alec Marx Gabriel Belen , DLSU ID# 12413704
 
 /*
     Description:  Resistor Color Calculator
-    Programmed by: Nono, Alec Marx Gabriel B. 
-    Last modified: <date when last revision was made>
-    Version: <version number>
-    [Acknowledgements: Stack Overflow, w3schools, YouTube]
+    Programmed by: Nono, Alec Marx Gabriel B.
+    Last modified: November 18, 2024
+    Version: v1.14
+    [Acknowledgements: Stack Overflow, w3schools, YouTube, GitHub]
 */
 
 #include <stdio.h>
@@ -25,7 +25,7 @@ NONO, Alec Marx Gabriel Belen , DLSU ID# 12413704
 #define SWHITEBOLD "\e[1;38;5;255m"
 #define SREDBOLD   "\e[1;38;5;196m"
 
-#define SBLACK    "\e[1;38;5;16mblack\e[39;0m"
+#define SBLACK    "\e[1;38;5;16;48;5;15mblack\e[39;0m"
 #define SBROWN    "\e[1;38;5;130mbrown\e[39;0m"
 #define SRED      "\e[1;38;5;196mred\e[39;0m"
 #define SORANGE   "\e[1;38;5;208morange\e[39;0m"
@@ -38,7 +38,7 @@ NONO, Alec Marx Gabriel Belen , DLSU ID# 12413704
 #define SGOLD     "\e[1;38;5;178mgold\e[39;0m"
 #define SSILVER   "\e[1;38;5;246msilver\e[39;0m"
 
-/* 
+/*
     Welcome lines with proper formatting; shows Key Legend
     Precondition: Program has just started
 */
@@ -56,7 +56,7 @@ void welcomeLine()
     printf("%19s\n", "Legend:");
     printf("%10s%20s\n","Color", "Key Assignment");
 
-    printf("%29s%16s\n", SBLACK, "B or b");
+    printf("%37s%16s\n", SBLACK, "B or b");
     printf("%30s%16s\n", SBROWN, "N or n");
     printf("%29s%17s\n", SRED,  "R or r");
     printf("%31s%15s\n", SORANGE,  "O or o");
@@ -71,27 +71,25 @@ void welcomeLine()
     printf("%11s%15s\n\n", "No Band", "Z or z");
 
 }
-/* 
+/*
     Asks the format of user input
     Precondition: welcomeLine() has already been called
     @param pFormat is used to store the state of the chosen mode
 */
 void askMode(char *pFormat)
 {
-    do
-    {
+    do{
         printf("Per-line mode [0] or One-liner mode [1]\t: ");
         scanf(" %c%*[^\n]",pFormat);
 
-        if (*pFormat != '0' && *pFormat != '1')
-        {
+        if (*pFormat != '0' && *pFormat != '1'){
             printf("%sInvalid input. Please enter 0 or 1.%s \n", SREDBOLD, SRESET);
         }
 
     } while (*pFormat != '0' && *pFormat != '1');
 }
 
-/* 
+/*
     Prints the user-entered bands
     Precondition: the characters used to represent the bands have been entered by the user
     @param nValid used to store the state of the validity of user input
@@ -113,8 +111,7 @@ void enteredBands(int *nValid, char *pBand1, char *pBand2,
 
     for(int i = 0; i <= 5; i++){
 
-        switch (*pBand)
-        {
+        switch (*pBand){
         case 'B': case 'b':
             printf("%s", SBLACK);
             break;
@@ -164,7 +161,7 @@ void enteredBands(int *nValid, char *pBand1, char *pBand2,
         pBand--;
     }
 }
-/* 
+/*
     Acquires the bands given through user input within a single line separated by commas
     Precondition: askMode() loop has already been exited
     @param nValid used to store the state of the validity of user input
@@ -222,7 +219,7 @@ void perLineInput(int *nValid, char *pBand1, char *pBand2,
                  pBand3, pBand4, 
                  pBand5, pBand6);
 }
-/* 
+/*
     Computes for the significant digits of the resistance
     Precondition: Either perLineInput() or oneLineInput()
                   have been called
@@ -245,8 +242,7 @@ int compute1stDigits(char *pBand1, int *pValid, int nDigits)
 
     for(int i = 0; i < nDigits; i++){
 
-        switch (*pBand)
-        {
+        switch (*pBand){
         case 'B': case 'b':
             *pDigit = 0;
             break;
@@ -307,7 +303,7 @@ int compute1stDigits(char *pBand1, int *pValid, int nDigits)
     }
 }
 
-/* 
+/*
     Computes for the resistance value
     Precondition: The significant digits of the resistance
                   have been computed and stored in a
@@ -321,8 +317,7 @@ float computeResistorVal(int nDigit123, char *pBand)
 {
     float fMultiplier;
 
-    switch (*pBand)
-        {
+    switch (*pBand){
         case 'B': case 'b':
             fMultiplier = 1;
             break;
@@ -358,19 +353,18 @@ float computeResistorVal(int nDigit123, char *pBand)
 
     return nDigit123 * fMultiplier;
 }
-/* 
+/*
     Computes for the resistor's tolerance
     Precondition: 3rd or 4th variable contain a
                   character corresponding to a band
     @param pBand used to determine multiplier value
-           used to calculate
+           which is then calculated
     @return a tolerance value is returned
 */
 float computeTolerance(char *pBand)
 {
 
-    switch (*pBand)
-        {
+    switch (*pBand){
         case 'N': case 'n':
             return 0.01;
             break;
@@ -392,16 +386,24 @@ float computeTolerance(char *pBand)
         case 'S': case 's':
             return 0.10; 
             break;
-        default:    
+        default:
             return 0;
             break;
         }
 }
+/*
+    Computes for the temperature coefficient
+    Precondition: 6th variable contains a valid 
+                  band that isn't set to 'No Band'
+    @param pBand used to determine temperature
+           coefficient value which is then 
+           calculated
+    @return a tolerance value is returned
+*/
+int computeTempCoefficient(char *pBand)
+{
 
-int computeTempCoefficient(char *pBand){
-
-    switch (*pBand)
-        {
+    switch (*pBand){
         case 'B': case 'b':
             return 250; 
             break;
@@ -434,7 +436,7 @@ int computeTempCoefficient(char *pBand){
             break;
         }
 }
-/* 
+/*
     Computes for the lower bound of the resistance
     Precondition: resistance value and tolerance have
                   been computed already
@@ -447,7 +449,7 @@ float rangeLowerBound(float fResistorVal, float fTolerance)
 {
     return fResistorVal - (fResistorVal * fTolerance);
 }
-/* 
+/*
     Computes for the upper bound of the resistance
     Precondition: resistance value and tolerance have
                   been computed already
@@ -460,8 +462,16 @@ float rangeUpperBound(float fResistorVal, float fTolerance)
 {
     return fResistorVal + (fResistorVal * fTolerance);
 }
-
-void output(float fResistorVal, int nDigit123, float fTolerance)
+/*
+    Prints pre-formatted output without temperature coefficient
+    Precondition: resistance value and tolerance have
+                  been computed already;
+                  rangeUpperBound() & rangeLowerBound
+                  have been declared
+    @param fResistorVal used to print out the value
+    @param fTolerance used to print out the value
+*/
+void output(float fResistorVal, float fTolerance)
 {
 
     if((fTolerance != 0) && (fResistorVal != 0)){
@@ -482,37 +492,32 @@ void output(float fResistorVal, int nDigit123, float fTolerance)
                     rangeUpperBound(fResistorVal, fTolerance),
                     SRESET);
         // Warning for resistors above 1000000 ohms
-        if(fResistorVal > 1000000)
-        {
+        if(fResistorVal > 1000000){
             printf("%sWarning! This value is above 100 Mega-ohms. No Practical Application! %s\n",
                     SREDBOLD,
                     SRESET);
         } 
 
     }
-    else if ((fTolerance == 0) && (fResistorVal == 0))
-    {
+    else if ((fTolerance == 0) && (fResistorVal == 0)){
         printf("%sInvalid Multiplier and Tolerance Band!%s\n", SREDBOLD, SRESET); 
     }
-    else if (fResistorVal == 0)
-    {
+    else if (fResistorVal == 0){
         printf("%sInvalid Multiplier Band!%s\n", SREDBOLD, SRESET);
     }
-    else
-    {
+    else{
         printf("%sAn error has occurred%s", SREDBOLD, SRESET);
     }
-     
+
 }
 
-void sixBandOutput(int nTempCoefficient){
+void sixBandOutput(int nTempCoefficient)
+{
 
-    if ((nTempCoefficient == 0))
-    {
+    if ((nTempCoefficient == 0)){
         printf("%sInvalid Temperature Coefficient Value%s\n", SREDBOLD, SRESET);
     }
-    else
-    {
+    else{
         printf("The temperature coefficient is %s%dppm/K%s\n", 
                 SWHITEBOLD,
                 nTempCoefficient,
@@ -543,53 +548,51 @@ int main()
 
 
     // executes the corresponding input mode 
-    if(cFormat == '0')
-    {
+    if(cFormat == '0'){
+
         perLineInput(&nValid, &cBand1, &cBand2, 
                      &cBand3, &cBand4, 
                      &cBand5, &cBand6);
     }
-    else if (cFormat == '1')
-    {
+    else if (cFormat == '1'){
+
         oneLineInput(&nValid, &cBand1, &cBand2, 
                      &cBand3, &cBand4, 
                      &cBand5, &cBand6);
     }
  
-    if(nValid == 0)
-    {
+    if(nValid == 0){
         printf("%sPlease use a valid input%s", SREDBOLD, SRESET);
     }
-    else
-    {
+    else{
         // flow if only 4 bands
         if((cBand5 == 'Z' || cBand5 == 'z') && 
-            (cBand6 == 'Z' || cBand6 =='z') )
-        {
+           (cBand6 == 'Z' || cBand6 == 'z')){
+
             nDigit123 = compute1stDigits(&cBand1, &nValid, 2);
             fResistorVal = computeResistorVal(nDigit123, &cBand3);
             fTolerance = computeTolerance(&cBand4);
 
-            output(fResistorVal, nDigit123, fTolerance);
+            output(fResistorVal, fTolerance);
         }
         // flow if only 5 bands
-        else if((cBand6 == 'Z') || (cBand6 == 'z'))
-        {
+        else if((cBand6 == 'Z') || (cBand6 == 'z')){
+
             nDigit123 = compute1stDigits(&cBand1, &nValid, 3);
             fResistorVal = computeResistorVal(nDigit123, &cBand4);
             fTolerance = computeTolerance(&cBand5);
 
-            output(fResistorVal, nDigit123, fTolerance);
+            output(fResistorVal, fTolerance);
         }
         // flow if there are 6 bands
-        else
-        {
+        else{
+
             nDigit123 = compute1stDigits(&cBand1, &nValid, 3);
             fResistorVal = computeResistorVal(nDigit123, &cBand4);
             fTolerance = computeTolerance(&cBand5);
             nTempCoefficient = computeTempCoefficient(&cBand6);
 
-            output(fResistorVal, nDigit123, fTolerance);
+            output(fResistorVal, fTolerance);
             sixBandOutput(nTempCoefficient);
         }
     }
